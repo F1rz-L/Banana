7250be6a9e2e1c975e54f9e8a4e271b3fac264e2
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-09-23 18:49:32 +0700

    Initial commit

---
+++ src/main/java/firzmc/banana/BananaModClient.java
@@ -0,0 +1,10 @@
+package firzmc.banana;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BananaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/firzmc/banana/BananaModDataGenerator.java
@@ -0,0 +1,11 @@
+package firzmc.banana;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BananaModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
